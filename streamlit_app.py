231db31d9041b5afeaa4ed880f0bee3a63cbb259
Duy Florent Pham-Anh <104060069+DF-PA@users.noreply.github.com>
2022-04-22 15:04:57 +0200

    Update streamlit_app.py
    korrektur

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ except URLerror as e:
 streamlit.stop()
 
 streamlit.header("The Fruit-Load-List contains:")
-def get_fruit_load_list:
+def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * from fruit_load_list")
     return my_cur.fetchall()
